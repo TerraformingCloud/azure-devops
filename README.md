13148ef3a111fc018c1158dce6acedde8f94bec9
Vamsikrishna Burugadda <65275624+TerraformingCloud@users.noreply.github.com>
2020-05-17 19:22:08 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+# Azure with Terraform 
+
+*Automating Infrastructure in Azure using Terraform scripts and ADO Pipelines*
+
+Click [Here](https://www.terraform.io/docs/providers/azurerm/guides/azure_cli.html) to see all the authentication methods
---
+++ outs.tf
@@ -0,0 +1,86 @@
+# #*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*
+# # Root Folder - Outputs
+# #*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*
+
+# # Network Outputs
+# #*-----------------------------------------------*#
+
+// output "rgname" {
+//     value       =   module.vnet.rgname
+// }
+
+// output "vnet_name" {
+//     value       =   module.vnet.vnet_name
+// }
+
+// output "vnet_location" {
+//     value       =   module.vnet.vnet_location
+// }
+
+// output "vnet_address_space" {
+//     value       =    module.vnet.vnet_address_space
+// }
+
+// output "subnet_names" {
+//     value       =   module.vnet.subnet_names
+// }
+
+// output "vnet_subnets" {
+//   value       =     module.vnet.vnet_subnets
+// }
+
+// output "nsg_names" {
+//     value       =   module.vnet.nsg_names
+// }
+
+
+# # Winvm Outputs
+# #*-----------------------------------------------*#
+
+//  output "win_nic_ids" {
+//      value       =   module.winvm.win_nic_ids
+//  }
+
+//  output "win_vm_names" {
+//      value       =   module.winvm.win_vm_names
+//  }
+
+//  output "win_vm_ids" {
+//      value       =   module.winvm.win_vm_ids
+//  }
+
+# # LinuxVM Outputs 
+# #*-----------------------------------------------*#
+
+# output "linux_nic_id" {
+#     value       =   module.linuxvm.linux_nic_id
+# }
+
+# output "linux_vm_name" {
+#     value       =   module.linuxvm.linux_vm_name
+# }
+
+# output "linux_vm_id" {
+#     value       =   module.linuxvm.linux_vm_id
+# }
+
+# LBR Linux VM Outputs 
+#*-----------------------------------------------*#
+
+// output "lbr_vm_nic_ids" {
+//     value       =   module.lbrvm.lbr_vm_nic_ids
+// }
+
+// output "lbr_vm_names" {
+//     value       =   module.lbrvm.lbr_vm_names
+// }
+
+// output "lbr_vm_ids" {
+//     value       =   module.lbrvm.lbr_vm_ids
+// }
+
+
+# FW-LBR Outputs 
+#*-----------------------------------------------*#
+
+// output ""
