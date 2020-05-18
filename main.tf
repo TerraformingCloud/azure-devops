0bc99f70c2b685ed01173e5b883a16c687ac12bf
Vamsikrishna Burugadda <65275624+TerraformingCloud@users.noreply.github.com>
2020-05-18 10:37:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,35 +20,3 @@ resource "azurerm_resource_group" "rg" {
     name        =       "TestRG"
     location    =       "South India"
 }
-
-// module "vnet" {
-//     source                  =   "./vnet"
-//     // rgname                  =   "testmodule"
-//     // location                =   "south india"
-//     // vnet_cidr               =   "172.16.0.0/16"
-//     // subnetCIDR              =   ["172.16.1.0/24", "172.16.2.0/24", "172.16.3.0/24"]
-// }
-
-// module "lbrvm" {
-//     source  =   "./api-vm"
-// }
-
-# module "keyvault" {
-#     source  =   "./keyvault"
-# }
-
-// module "winvm" {
-//      source  =   "./winVM"
-// }
-
-// module "bastion" {
-//     source   =   "./bastion"
-// }
-
-// module "linuxvm" {
-//      source  =   "./linuxVM"
-//  }
-
-//  module "fw-lbr" {
-//     source  =   "./fw-lbr"
-// }
